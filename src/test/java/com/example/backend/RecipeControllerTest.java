834969a0467698f02879742c268a5825c5f99fe2
Johann Mendoza <johannmendoza370@gmail.com>
2024-05-14 17:09:56 -0500

    integrated mysql database

--- src/test/java/com/example/backend/RecipeControllerTest.java
+++ src/test/java/com/example/backend/RecipeControllerTest.java
@@ -40,37 +40,41 @@ public class RecipeControllerTest {
     @Test
     void shouldGetAllRecipes() throws Exception {
         List<Recipe> recipes = new ArrayList<>();
-        recipes.add(new Recipe(1L, "test name 1", "test url 1"));
-        recipes.add(new Recipe(2L, "test name 2", "test url 2"));
-        recipes.add(new Recipe(3L, "test name 3", "test url 3"));
+        recipes.add(new Recipe(1L, "test name 1", "test url 1", "image url 1"));
+        recipes.add(new Recipe(2L, "test name 2", "test url 2", "image url 2"));
+        recipes.add(new Recipe(3L, "test name 3", "test url 3", "image url 3"));
         when(recipeRepository.findAll()).thenReturn(recipes);
         this.mockMvc.perform(get("/recipes"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].id").value(1))
                 .andExpect(jsonPath("$[0].name").value("test name 1"))
                 .andExpect(jsonPath("$[0].url").value("test url 1"))
+                .andExpect(jsonPath("$[0].imageUrl").value("image url 1"))
                 .andExpect(jsonPath("$[1].id").value(2))
                 .andExpect(jsonPath("$[1].name").value("test name 2"))
                 .andExpect(jsonPath("$[1].url").value("test url 2"))
+                .andExpect(jsonPath("$[1].imageUrl").value("image url 2"))
                 .andExpect(jsonPath("$[2].id").value(3))
                 .andExpect(jsonPath("$[2].name").value("test name 3"))
-                .andExpect(jsonPath("$[2].url").value("test url 3"));
+                .andExpect(jsonPath("$[2].url").value("test url 3"))
+                .andExpect(jsonPath("$[2].imageUrl").value("image url 3"));
     }
 
     @Test
     void shouldGetRecipeById() throws Exception {
-        when(recipeRepository.findById(1L)).thenReturn(Optional.of(new Recipe(1L, "test recipe", "test url")));
+        when(recipeRepository.findById(1L)).thenReturn(Optional.of(new Recipe(1L, "test recipe", "test url", "test image url")));
         this.mockMvc.perform(get("/recipes/1"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1))
                 .andExpect(jsonPath("$.name").value("test recipe"))
-                .andExpect(jsonPath("$.url").value("test url"));
+                .andExpect(jsonPath("$.url").value("test url"))
+                .andExpect(jsonPath("$.imageUrl").value("test image url"));
     }
 
     @Test
     void shouldAddRecipes() throws Exception {
-        Recipe recipe = new Recipe("test", "test url");
-        Recipe returnedRecipe = new Recipe( 1L, "test", "test url");
+        Recipe recipe = new Recipe("test", "test url", "test image url");
+        Recipe returnedRecipe = new Recipe( 1L, "test", "test url", "test image url");
         when(recipeRepository.save(recipe)).thenReturn(returnedRecipe);
 
         JSONObject json = new JSONObject();
@@ -82,18 +86,20 @@ public class RecipeControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").value(1))
                 .andExpect(jsonPath("$.name").value("test"))
-                .andExpect(jsonPath("$.url").value("test url"));
+                .andExpect(jsonPath("$.url").value("test url"))
+                .andExpect(jsonPath("$.imageUrl").value("test image url"));
     }
 
     @Test
     void shouldNotAddDuplicateRecipe() throws Exception {
-        Recipe recipe = new Recipe("test", "test url");
-        Recipe returnedRecipe = new Recipe( 1L, "test", "test url");
+        Recipe recipe = new Recipe("test", "test url", "test image url");
+        Recipe returnedRecipe = new Recipe( 1L, "test", "test url", "test image url");
         when(recipeRepository.existsByNameAndUrl(recipe.getName(), recipe.getUrl())).thenReturn(true);
 
         JSONObject json = new JSONObject();
         json.put("name", "test");
         json.put("url", "test url");
+        json.put("imageUrl", "test image url");
         this.mockMvc.perform(post("/recipes")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json.toString()))
@@ -136,7 +142,7 @@ public class RecipeControllerTest {
 
     @Test
     void shouldDeleteRecipeById() throws Exception {
-        when(recipeRepository.findById(1L)).thenReturn(Optional.of(new Recipe("test name", "test url")));
+        when(recipeRepository.findById(1L)).thenReturn(Optional.of(new Recipe("test name", "test url", "test image url")));
         this.mockMvc.perform(delete("/recipes/1"))
                 .andExpect(status().isNoContent());
     }
